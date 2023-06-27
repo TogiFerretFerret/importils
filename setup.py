12cd68c99f9409129a9b4e7ba1ec402b6d544eb6
TogiFerretFerret <hreich29@riverdale.edu>
2023-06-27 17:12:07 -0400

    idk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'importils',         # How you named your package folder (MyLib)
   packages = ['importils'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
